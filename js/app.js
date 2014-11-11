efc9005fad7619de4197eeed3e3b192503c1a87a
tdog3000 <tdog3000@users.noreply.github.com>
2014-11-11 10:24:54 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,55 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+Query(document).foundation();
+
+jQuery(document).ready(function($) {
+
+    /* Mobile Button
+    ===================================== */
+    $( '.mobile-btn' ).click( function(){
+        $( '.site-navigation' ).slideToggle( 200 );
+        event.preventDefault();
+    } );
+
+    /* Submenu
+    ===================================== */
+
+   $( '.menu-item-has-children > a' ).click( function(){
+        var $parent = $(this).parent(),
+            $child = $parent.children( 'ul' );
+            $child.toggleClass( 'active' );
+            $parent.toggleClass('open');
+            $(  '.menu-item-has-children' ).not($parent).removeClass('open');
+            $( '.menu-item-has-children > ul').not($child).removeClass('active');
+         if (event.preventDefault) { event.preventDefault(); } else { event.returnValue = false; } 
+    });
+    
+    
+    /* Setting the cookies
+    ===================================== */
+
+    // for top bar
+    $('.cc-cookie-accept').click(function (e) {
+        e.preventDefault();
+        $.cookie("cc_cookie_accept", "cc_cookie_accept", {
+            expires: 365,
+            path: '/'
+        });
+
+        $("#cookie").fadeOut(function () {
+            // reload page to activate cookies
+            location.reload();
+        });
+    });
+
+    //reset cookies
+    $('a.cc-cookie-reset').click(function (f) {
+        f.preventDefault();
+        $.cookie("cc_cookie_accept", null, {
+            path: '/'
+        });
+        $("#cookie").fadeIn(function () {
+            // reload page to activate cookies
+            location.reload();
+        });
+    });
+    
+});
