7769f72808f607d560f745b42a8aeb22503ac149
tdog3000 <tdog3000@users.noreply.github.com>
2014-11-12 09:36:14 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,6 +6,9 @@
  *
  * @package _s
  */
+
+global $shortname;
+
 ?>
 
     </div><!-- #content -->
@@ -33,9 +36,9 @@
         <div class="site-copyright">
             <ul>
                 <li><p>&copy; <?php echo the_date('Y'); ?> <a href="<?php bloginfo('url');?>" rel="index"><?php bloginfo('name'); ?></a></p></li>
-                 <?php if( get_option( 'iw_privacy' ) ) { ?><li><a href="<?php echo get_permalink( get_option( 'iw_privacy' ) ); ?>"><?php echo get_the_title( get_option( 'iw_privacy' ) ); ?></a></li><?php } ?>
-                 <?php if( get_option( 'iw_cookie' ) ) { ?><li><a href="<?php echo get_permalink( get_option( 'iw_cookie' ) ); ?>"><?php echo get_the_title( get_option( 'iw_cookie' ) ); ?></a></li><?php } ?>
-                <li><p>Design by <a href="http://www.inkandwater.co.uk/" target="_blank" rel="bookmark">Ink &amp; Water</a></p></li>
+                 <?php if( get_option( 'iw_privacy' ) ) { ?><li><a href="<?php echo get_permalink( get_option( $shortname . '_privacy' ) ); ?>"><?php echo get_the_title( get_option( $shortname . '_privacy'  ) ); ?></a></li><?php } ?>
+                 <?php if( get_option( 'iw_cookie' ) ) { ?><li><a href="<?php echo get_permalink( get_option( $shortname . '_cookie' ) ); ?>"><?php echo get_the_title( get_option( $shortname . '_cookie'  ) ); ?></a></li><?php } ?>
+                <li><p><?php _e('Design by <a href="http://www.inkandwater.co.uk/" target="_blank" rel="bookmark">Ink &amp; Water</a>');?></p></li>
             </ul>
         </div><!-- .site-copyright -->
 
