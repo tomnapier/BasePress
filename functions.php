f970e767ab9de2d7809cc1a6006feb58ef3a48dd
Tom Napier <tom.napier21@gmail.com>
2015-02-23 21:29:03 +0000

    Removed unnecessary functions files

--- functions.php
+++ functions.php
@@ -3,16 +3,10 @@
 /* Functions
 ============ */
 
-require_once( 'inc/functions/audio.php' );
-require_once( 'inc/functions/content.php' );
 require_once( 'inc/functions/cookie.php' );
-require_once( 'inc/functions/essentials.php' );
-require_once( 'inc/functions/homecategory.php' );
 require_once( 'inc/functions/menus.php' );
 require_once( 'inc/functions/meta-boxes.php' );
-require_once( 'inc/functions/page-numbers.php' );
 require_once( 'inc/functions/scripts.php' );
-require_once( 'inc/functions/shortcodes.php' );
 require_once( 'inc/functions/thumbs.php' );
 require_once( 'inc/functions/tinymce.php' );
 require_once( 'inc/functions/template-tags.php' );
